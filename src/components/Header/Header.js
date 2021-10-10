ce27ddf7454d2750a0a4380df2cee22e02146f8f
Jabed Hossain <hossainjabed3@gmail.com>
2021-10-11 02:06:19 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            header is coming
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            its home minister
+        </div>
+    );
+};
+
+export default Home;
